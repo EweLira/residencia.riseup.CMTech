d66f91ced22489b389ca67c33d704c8c0091c740
Ewellyn Lira <ewellynflira@gmail.com>
2022-09-25 21:21:18 -0300

    criação de aula3

---
+++ arrays.js
@@ -0,0 +1,6 @@
+let frutas = ['maçã','laranja','uva']
+
+console.log(frutas[1])
+console.log(frutas.length)
+
+frutas.pop()
---
+++ exercicio 2/scripts.js
@@ -0,0 +1,18 @@
+var titulo, descricao, listaItens, lista, quadrados, itens;
+function frutas(){
+    alert("Conseguimos!");
+    var titulo = document.getElementById("titulo");
+    titulo.innerHTML="Titulo";
+   
+    var descricao = document.getElementById("descricao");
+    descricao.innerHTML="Uma lista de frutas";
+ 
+
+   
+   
+}
+ 
+ 
+ 
+ 
+S

e4583716e4d0ebb082b5ec0cfb8017cb26c06c8c
Ewellyn Lira <113145267+EweLira@users.noreply.github.com>
2022-12-02 13:53:24 -0300

    Update scripts.js

--- exercicio 2/scripts.js
+++ exercicio 2/scripts.js
@@ -1,6 +1,17 @@
-var titulo, descricao, listaItens, lista, quadrados, itens;
-function frutas(){
-    alert("Conseguimos!");
+var titulo = document.querySelector("#titulo").innerHTML;
+var descricao = document.querySelector("#descricao").innerHTML;
+var listaItens = document.querySelector(".itens");
+var  lista = document.querySelector("#lista");
+var quadrados = document.querySelector("#quadrados");
+let frutas =["Maçã","Banana","Uva","Abacaxi"];
+
+listaItens[2].innerHTML = "Caju";
+
+quadrados.innerHTML = "<div id='q1' style='background: red;width: 100px;height: 100px'></div>"
+quadrados.innerHTML += "<div id='q2' style='background: blue;width: 200px;height: 200px; margin-top: 10px' ></div>"
+
+
+    
     var titulo = document.getElementById("titulo");
     titulo.innerHTML="Titulo";
    

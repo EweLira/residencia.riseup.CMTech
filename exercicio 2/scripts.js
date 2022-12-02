8d8e496460b7736a45619e6c5450b5bd1e586cd2
Ewellyn Lira <113145267+EweLira@users.noreply.github.com>
2022-12-02 14:20:20 -0300

    atualizado ex2

--- exercicio 2/scripts.js
+++ exercicio 2/scripts.js
@@ -1,29 +1,21 @@
-var titulo = document.querySelector("#titulo").innerHTML;
+var titulo = document.querySelector("h1").innerHTML;
 var descricao = document.querySelector("#descricao").innerHTML;
-var listaItens = document.querySelector(".itens");
-var  lista = document.querySelector("#lista");
+var listaItens = document.querySelectorAll(".itens");
+var lista = document.querySelector("#lista");
+var listaFrutas = document.querySelector("#frutas");
 var quadrados = document.querySelector("#quadrados");
 let frutas =["Maçã","Banana","Uva","Abacaxi"];
 
 listaItens[2].innerHTML = "Caju";
-
-quadrados.innerHTML = "<div id='q1' style='background: red;width: 100px;height: 100px'></div>"
-quadrados.innerHTML += "<div id='q2' style='background: blue;width: 200px;height: 200px; margin-top: 10px' ></div>"
+alert("Terceiro elemento mudou");
 
 
-    
-    var titulo = document.getElementById("titulo");
-    titulo.innerHTML="Titulo";
-   
-    var descricao = document.getElementById("descricao");
-    descricao.innerHTML="Uma lista de frutas";
- 
+lista.innerHTML="<li>Chocolate</li>"
+lista.innerHTML+="<li>Bolo</li>"
 
-   
-   
+quadrados.innerHTML = "<div id='q1' style='background-color: red;width: 100px;height: 100px'></div>"
+quadrados.innerHTML += "<div id='q2' style='background-color: blue;width: 200px;height: 200px; margin-top: 10px' ></div>"
+
+for(i = 0;i < frutas.length;i++){
+    listaFrutas.innerHTML += "<li>"+frutas[i]+"</li>"
 }
- 
- 
- 
- 
-S
